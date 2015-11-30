1d2855fef7c849a6c4745d60f1dc353967056226
benthieu <mathieu.b93@gmail.com>
2015-11-30 19:17:20 +0100

    - Settings App Bar Icon

--- app/src/main/java/com/example/thieu/hunger/SettingsActivity.java
+++ app/src/main/java/com/example/thieu/hunger/SettingsActivity.java
@@ -2,6 +2,11 @@ package com.example.thieu.hunger;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.thieu.hunger.db.object.User;
 
 
 public class SettingsActivity extends Activity {
@@ -14,5 +19,11 @@ public class SettingsActivity extends Activity {
 		getFragmentManager().beginTransaction()
                 .replace(android.R.id.content,new SettingsFragment())
                 .commit();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getActionBar().setIcon(R.drawable.main_logo);
+		return true;
 	}
 }
